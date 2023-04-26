af62f3d539b9577e91f57792a4bf75826e091f1d
Valery-Bolotin <bolotinval86@gmail.com>
2023-04-26 21:26:51 +0300

    feat: add html wrapper and main elements

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -1,4 +1,22 @@
+let wrapper = document.createElement('div');
+wrapper.className = "wrapper";
+document.body.append(wrapper);
+
+
 let title = document.createElement('h1');
 title.className = "title";
 title.innerHTML = "RSS Виртуальная клавиатура";
-document.body.append(title);
+wrapper.prepend(title);
+
+let textarea = document.createElement('textarea');
+textarea.className = "textarea";
+title.after(textarea);
+
+let keyboard = document.createElement('div');
+keyboard.className = "keyboard";
+textarea.after(keyboard);
+
+let info = document.createElement('div');
+info.className = "info";
+info.innerHTML = "Клавиатура создана в операционной системе Windows <br> Для переключения языка используйте сочетание клавишь:";
+keyboard.after(info);
