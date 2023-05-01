c1f29830b86cdc9133cdf14538b5af1aa4c3fa44
Valery-Bolotin <bolotinval86@gmail.com>
2023-05-01 19:23:44 +0300

    feat: add fuction choice lang

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -18,43 +18,57 @@ textarea.after(keyboard);
 
 let info = document.createElement('div');
 info.className = "info";
-info.innerHTML = "Клавиатура создана в операционной системе Windows <br> Для переключения языка используйте сочетание клавишь:";
+info.innerHTML = "Клавиатура создана в операционной системе Windows <br> Для переключения языка используйте сочетание клавишь: Ctrl+ Alt";
 keyboard.after(info);
 
 let virtualKeyboard = document.querySelector(".keyboard");
 let outputArea = document.querySelector(".textarea");
 
+
 const ruKeys = ['ё','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'CapsLoack', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#9650;', 'Shift', 'Ctrl', 'Win', 'Alt', ' ','Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl' ];
 const enKeys = [ '`','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'CapsLoack', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "' ", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#9650;', 'Shift', 'Ctrl', 'Win', 'Alt', ' ','Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'];
 
 
-let n =0;
+let lang = 'eng';
 
-let language = 'en';
 
-for (let i = 0; i < 64; i++) {
-    const key = document.createElement('div');
+function createDivs() {
+  for(let i = 0; i < 64; i++) {
+    const key = document.createElement('div'); // создаем div
     key.className = 'key';
+    key.id = `key-${i}`; // присваиваем id вида "key-0", "key-1" и т.д.
+    keyboard.append(key); 
     
-   if (language === 'en'){
-    key.innerHTML = enKeys[n];
-    n += 1;
-   }
-    else if (language === 'ru'){
-      key.innerHTML = ruKeys[n];
-    n += 1;
-    }
-    keyboard.append(key);
   }
-  // Обрабатываем нажатия на клавиши клавиатуры
-virtualKeyboard.addEventListener("click", function(event) {
-    // Получаем символ, соответствующий нажатой клавише
-   if(event.target.innerHTML !== 'Backspace'){
-    let keyPressed = event.target.innerText;
-    
-    // Отображаем символ в области вывода
-    outputArea.innerHTML += keyPressed;
+  
 }
+function fillDivs() {
+  const keyboard = lang === 'rus' ? ruKeys : enKeys; // выбираем нужный массив клавиш в зависимости от языка
+  const divs = document.querySelectorAll('.key'); 
+  divs.forEach((key, index) => {
+    key.innerHTML = keyboard[index % keyboard.length]; 
+  });
+}
+
+
+// обработчик нажатия клавиш Ctrl+Alt
+document.addEventListener('keydown', (event) => {
+  if(event.ctrlKey && event.altKey) { 
+    lang = lang === 'rus' ? 'eng' : 'rus'; 
+    fillDivs(); 
+  }
+});
+createDivs();
+fillDivs();
+
+  // Обрабатываем нажатия на клавиши клавиатуры
+  virtualKeyboard.addEventListener("click", function(event) {
+    let keyClick = event.target.innerHTML; 
+    if(keyClick !== 'Backspace' && keyClick !== 'Tab' && keyClick !== 'Del' && keyClick !== 'CapsLoack' && keyClick !== 'Enter'&& keyClick !== 'Shift' && keyClick !==' ' && keyClick !=='Alt' && keyClick !=='&#9668;' && keyClick !== '&#9660;' && keyClick !== '&#9658;' && keyClick !== 'Ctrl'){
+    let keyPressed = event.target.innerText;
+    outputArea.innerHTML += keyPressed;
+    event.preventDefault();
+  }
 });
 
 virtualKeyboard.addEventListener('keydown', function(event) {
@@ -66,13 +80,10 @@ virtualKeyboard.addEventListener('keydown', function(event) {
 });
   
 
-// Добавим слушатель события keyup
 virtualKeyboard.addEventListener("click", function(event) {
-  // Проверим нажатие клавиши Backspace
-  if (event.target.innerHTML === 'Backspace') {
+    if (event.target.innerHTML === 'Backspace') {
     let value = outputArea.value;
     outputArea.value = value.substring(0, value.length - 1);
-    event.preventDefault();
   }
 });
 
@@ -83,3 +94,19 @@ function deleteLastChar(input) {
     input.value = value.substring(0, value.length - 1);
   }
 }
+
+document.addEventListener('keydown', (event) => {
+  const key = event.key;
+  const virtualKey = virtualKeyboard.querySelector(`.key[id="${key}"]`);
+  if (virtualKey) {
+    virtualKey.classList.add('selected');
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  const key = event.key;
+  const virtualKey = virtualKeyboard.querySelector(`.key[id="${key}"]`);
+  if (virtualKey) {
+    virtualKey.classList.remove('selected');
+  }
+});
