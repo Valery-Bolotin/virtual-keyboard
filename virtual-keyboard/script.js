47c2623c6fff78b824a933fe14bb46a27a18588b
Valery-Bolotin <bolotinval86@gmail.com>
2023-05-01 14:26:28 +0300

    feat: add special key for keyboard

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -21,32 +21,24 @@ info.className = "info";
 info.innerHTML = "Клавиатура создана в операционной системе Windows <br> Для переключения языка используйте сочетание клавишь:";
 keyboard.after(info);
 
-const globalKeys = ['`','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\\'];
-const ruKeys = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю'];
-const enKeys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "' ", 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'];
+
+const ruKeys = ['ё','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',  'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'CapsLoack', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#9650;', 'Shift', 'Ctrl', 'Win', 'Alt', ' ','Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl' ];
+const enKeys = [ '`','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',  'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'CapsLoack', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "' ", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#9650;', 'Shift', 'Ctrl', 'Win', 'Alt', ' ','Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'];
 
 
-let n =0;
-let l =0;
 
 for (let i = 0; i < 64; i++) {
     const key = document.createElement('div');
+    div.setAttribute("id", `div-${i+1}`);
     key.className = 'key';
-    
-  if(i >= 0 && i < 13 || i ===27){
-    key.innerHTML = globalKeys[l];
-    l += 1;
-  }
-  else if (i > 14 && i !== 27 && i !== 28 && i !== 29 && i !== 41 && i !== 42 && i <53){
-    key.innerHTML=enKeys[n]
-    n += 1;
-   }
     keyboard.append(key);
   }
 
   // Получаем ссылки на элементы клавиатуры и области вывода
 let virtualKeyboard = document.querySelector(".keyboard");
 let outputArea = document.querySelector(".textarea");
+
+
 
 // Обрабатываем нажатия на клавиши клавиатуры
 virtualKeyboard.addEventListener("click", function(event) {
